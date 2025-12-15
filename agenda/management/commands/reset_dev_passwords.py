03843c216f793b7badb3c07bf747e92db01cff60
Arthur Araújo <arthuraraujo07@hotmail.com>
2025-12-14 23:07:45 -0300

    feat: marketplace simples para freelancers

---
+++ agenda/management/commands/reset_dev_passwords.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Redefine senhas de desenvolvimento para sara, arthur e roberto (formato: <username>2025@)'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        users = ['sara', 'arthur', 'roberto']
+        updated = 0
+
+        for username in users:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                self.stdout.write(self.style.WARNING(f'Usuário {username} não encontrado.'))
+                continue
+
+            new_password = f'{username}2025@'
+            user.set_password(new_password)
+            user.save(update_fields=['password'])
+            updated += 1
+            self.stdout.write(self.style.SUCCESS(f'Senha redefinida para {username}.'))
+
+        self.stdout.write(self.style.SUCCESS(f'Total atualizado: {updated}'))
