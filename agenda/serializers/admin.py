88a2d523c4822291dd854af28ae0951d3a7efaee
Arthur Araújo <arthuraraujo07@hotmail.com>
2026-03-03 11:50:00 -0300

    fix(ci): ajusta ordem de imports no serializer admin

--- agenda/serializers/admin.py
+++ agenda/serializers/admin.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.core.validators import validate_email
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from ..models import (
